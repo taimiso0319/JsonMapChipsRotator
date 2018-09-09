032de331c15ab1baff1b07bc47dd84f17f0b2f1a
Keisuke Yasuda <keisuke0319@gmail.com>
2018-09-09 16:43:46 +0900

    add exceptions for too much lines. and now it outputs data that is rotated for 90 180 270 degree.

--- main.py
+++ main.py
@@ -18,20 +18,19 @@ def initialize():
     json_text = "jsondatas.txt"
     output_text = "output.txt"
 
-def rotate_right(jsondata, times):
+def rotate_right(jsondata):
       
     array_size=math.sqrt(len(jsondata['mapChip']))
     if not array_size.is_integer():
         return
-    s = int(array_size)
-    for r in range(times):      
-        tmp_data = copy.deepcopy(jsondata)
-        for i in range(len(jsondata['mapChip'])):
-            a = 99 - (i + ((s * (s-1)) - ((i%s) * (s+1)) - ((i // s) * (s-1))))
-            jsondata['mapChip'][a] = tmp_data['mapChip'][i]
-            if len(tmp_data['mapChip'][i]) > 0:
-                jsondata['mapChip'][a]['addrId'] ='mapChipAdr_'+ str(s - (i//s)) +'-'+ str(i%s+2)
-                jsondata['mapChip'][a]['mapDirectionType'] =  (tmp_data['mapChip'][i]['mapDirectionType'] + 90) % 360
+    s = int(array_size) 
+    tmp_data = copy.deepcopy(jsondata)
+    for i in range(len(jsondata['mapChip'])):
+        a = 99 - (i + ((s * (s-1)) - ((i%s) * (s+1)) - ((i // s) * (s-1))))
+        jsondata['mapChip'][a] = tmp_data['mapChip'][i]
+        if len(tmp_data['mapChip'][i]) > 0:
+            jsondata['mapChip'][a]['addrId'] ='mapChipAdr_'+ str(s - (i//s)) +'-'+ str(i%s+2)
+            jsondata['mapChip'][a]['mapDirectionType'] =  (tmp_data['mapChip'][i]['mapDirectionType'] + 90) % 360
 
     
 
@@ -48,16 +47,20 @@ if __name__ == "__main__":
     global json_text
     global output_text
     output = ""
+    if sum(1 for line in open(json_text)) > 5000:
+        print("too much lines for input, please make sure that has less than 5000 lines.")
+        sys.exit(main())
     with open(json_text, "r") as fin:
         c = 0
         for list in fin:
             jsondata = json.loads(list.strip())
-            rotate_right(jsondata,1)
-            output = output.join(str(jsondata)+'\n')
-    if not os.path.isfile(output_text):
+            for times in range(3):
+                rotate_right(jsondata)
+                output += str(jsondata)+'\n'
+    if os.path.exists(output_text):
         with open(output_text, "w") as fout:
             fout.write(output)
-    else:  
+    else:
         with open(output_text, "x") as fout:
             fout.write(output)
     
